f58c7f8529f6a2c066db04a181adcee1e46fe4bf
syihwamoza <mozakastella26@gmail.com>
2024-03-29 10:17:53 +0700

    memasukkan nilai vvariabel  i dan cmp_count

--- algo_29maret/algo_29maret.cpp
+++ algo_29maret/algo_29maret.cpp
@@ -56,3 +56,9 @@ void q_short(int low, int high)
 
 	while (i <= j) //step 10
 	{
+		//search for an element  greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) //step 5
+		{
+			i++; //step 6
+			cmp_count++;
+		}
