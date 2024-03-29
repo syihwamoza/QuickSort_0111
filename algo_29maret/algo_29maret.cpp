bfb85d8c66bcef9285a628c13a1238fe6e55b7ea
syihwamoza <mozakastella26@gmail.com>
2024-03-29 10:33:05 +0700

    mengurutkan arr kanan

--- algo_29maret/algo_29maret.cpp
+++ algo_29maret/algo_29maret.cpp
@@ -84,3 +84,6 @@ void q_short(int low, int high)
 
 	//recursive call to sort the left sub array
 	q_short(low, j - 1); //step 12
+
+	//recursive call to sort the right sub array
+	q_short(j + 1, high); //step 13
